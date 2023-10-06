26917df3aeda0f8afabe8a0d0d74c464708a4895
Alessandro Pacifici <ap.pacifici@gmail.com>
2023-10-06 17:44:14 +0000

    Creazione migration inizializzazione dati db per test

--- migrations/Version20231006130911.php
+++ migrations/Version20231006130911.php
@@ -22,6 +22,7 @@ final class Version20231006130911 extends AbstractMigration
         $this->addSql("INSERT INTO events (name,city,date_event) VALUE ('Cold Play Colorfull', 'Roma', '2024-07-22T21:00:00')");
         $this->addSql("INSERT INTO locations (name,address,event_id) VALUE ('Stadio Olimpico', 'Viale dei Gladiatori, 00135 Roma RM', 1)");
 
+        //Settore Tribuna ID 1 - Posti Totali 5
         $this->addSql("INSERT INTO sectors (name,total,purchased,place_type,event_id,location_id) VALUE (
                 'Tribuna D\'onore', 
                 '5', 
@@ -32,11 +33,67 @@ final class Version20231006130911 extends AbstractMigration
             )
         ");
 
+        //Settore Prato ID 2 - Posti totali 100
         $this->addSql("INSERT INTO sectors (name,total,purchased,place_type,event_id,location_id) VALUE (
-            'Tribuna D\'onore', 
+            'Prato', 
             '100', 
             0,
             1,
+            1,
+            1
+        )
+        ");
+
+        //Tribuna Fila 1 Posto 5A
+        $this->addSql("INSERT INTO places (line,number,price,free,event_id,sector_id) VALUE (
+            '1', 
+            '5A', 
+            50,
+            0,
+            1,
+            1
+        )
+        ");
+
+        //Tribuna Fila 1 Posto 6A
+        $this->addSql("INSERT INTO places (line,number,price,free,event_id,sector_id) VALUE (
+            '1', 
+            '6A', 
+            50,
+            0,
+            1,
+            1
+        )
+        ");
+
+        //Tribuna Fila 1 Posto 7A
+        $this->addSql("INSERT INTO places (line,number,price,free,event_id,sector_id) VALUE (
+            '1', 
+            '7A', 
+            50,
+            0,
+            1,
+            1
+        )
+        ");
+         
+        //Tribuna Fila 1 Posto 8A
+        $this->addSql("INSERT INTO places (line,number,price,free,event_id,sector_id) VALUE (
+            '1', 
+            '8A', 
+            50,
+            0,
+            1,
+            1
+        )
+        ");
+
+        //Tribuna Fila 1 Posto 9A
+        $this->addSql("INSERT INTO places (line,number,price,free,event_id,sector_id) VALUE (
+            '1', 
+            '9A', 
+            50,
+            0,
             1,
             1
         )
@@ -46,7 +103,11 @@ final class Version20231006130911 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql("set foreign_key_checks=0");
+        $this->addSql("TRUNCATE places");
+        $this->addSql("TRUNCATE sectors");
+        $this->addSql("TRUNCATE locations");
+        $this->addSql("TRUNCATE events");
+        $this->addSql("set foreign_key_checks=1");
     }
 }

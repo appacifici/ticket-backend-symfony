10ec23072f5c9cd5f840d61c517abcb0eb79abae
Alessandro Pacifici <ap.pacifici@gmail.com>
2023-10-09 21:16:26 +0000

    Rimossa cartella vendor inserita per errore su git

--- vendor/psalm/plugin-symfony/src/Exception/InvalidConsoleModeException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Psalm\SymfonyPsalmPlugin\Exception;
-
-class InvalidConsoleModeException extends \InvalidArgumentException
-{
-}
--- vendor/psalm/plugin-symfony/src/Exception/TemplateNameUnresolvedException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Psalm\SymfonyPsalmPlugin\Exception;
-
-class TemplateNameUnresolvedException extends \RuntimeException
-{
-}
